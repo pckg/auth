14ebe5fe5236e3ae237b3530ea7ea60e216102bd
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-09-23 16:28:15 +0200

    Remove 'defaults.' from config key

--- src/Pckg/Auth/Middleware/RestrictAccess.php
+++ src/Pckg/Auth/Middleware/RestrictAccess.php
@@ -9,7 +9,7 @@ class RestrictAccess extends AbstractChainOfReponsibility
     {
         $routeName = router()->get('name');
 
-        foreach (config('defaults.pckg.auth', []) as $gate) {
+        foreach (config('pckg.auth', []) as $gate) {
             /**
              * Check rules for logged-out users.
              */
