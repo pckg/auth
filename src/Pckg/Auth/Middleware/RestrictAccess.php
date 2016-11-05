e1b1940fcb28e9f764f2d01d93a35febad3f0c2e
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-11-05 12:04:22 +0100

    Rewrite restrict access

--- src/Pckg/Auth/Middleware/RestrictAccess.php
+++ src/Pckg/Auth/Middleware/RestrictAccess.php
@@ -1,57 +1,63 @@
 <?php namespace Pckg\Auth\Middleware;
 
 use Pckg\Concept\AbstractChainOfReponsibility;
+use Pckg\Concept\Reflect;
 
 class RestrictAccess extends AbstractChainOfReponsibility
 {
 
     public function execute(callable $next)
     {
-        $routeName = router()->get('name');
+        $router = router()->get();
+        $routeName = $router['name'];
 
         foreach (config('pckg.auth', []) as $gate) {
             /**
              * Check rules for logged-out users.
              */
-            if ($gate['status'] == 'logged-out' && !auth()->isLoggedIn()) {
-                /**
-                 * Check if route is excluded in rule.
-                 */
-                if (isset($gate['exclude']) && !in_array($routeName, $gate['exclude'])) {
-                    foreach ($gate['exclude'] as $route) {
-                        if (preg_match('#' . $route . '#', $routeName)) {
-                            break 2;
-                        }
-                    }
-                    
-                    redirect(url($gate['redirect']));
-                }
+            if ($gate['status'] == 'logged-out' && auth()->isLoggedIn()) {
+                continue;
 
-                /**
-                 * Check if route is included in rule.
-                 */
-                if (isset($gate['include']) && in_array($routeName, $gate['include'])) {
-                    redirect(url($gate['redirect']));
-                }
+            } elseif ($gate['status'] == 'logged-in' && !auth()->isLoggedIn()) {
+                continue;
+
             }
 
             /**
-             * Check rules for logged-in users.
+             * Check if route is excluded in rule.
              */
-            if ($gate['status'] == 'logged-in' && auth()->isLoggedIn()) {
-                /**
-                 * Check if route is excluded in rule.
-                 */
-                if (isset($gate['exclude']) && !in_array($routeName, $gate['exclude'])) {
-                    redirect(url($gate['redirect']));
+            if (isset($gate['exclude']) && !in_array($routeName, $gate['exclude'])) {
+                foreach ($gate['exclude'] as $route) {
+                    if (preg_match('#' . $route . '#', $routeName)) {
+                        break 2;
+                    }
                 }
 
-                /**
-                 * Check if route is included in rule.
-                 */
-                if (isset($gate['include']) && in_array($routeName, $gate['include'])) {
-                    redirect(url($gate['redirect']));
+                redirect(url($gate['redirect']));
+            }
+
+            /**
+             * Check if route is included in rule.
+             */
+            if (isset($gate['include']) && in_array($routeName, $gate['include'])) {
+                foreach ($gate['include'] as $route) {
+                    if (preg_match('#' . $route . '#', $routeName)) {
+                        break 2;
+                    }
                 }
+
+                redirect(url($gate['redirect']));
+            }
+
+            /**
+             * Check for callback.
+             */
+            if (isset($gate['callback'])) {
+                if (Reflect::method($gate['callback']['class'], $gate['callback']['method'])) {
+                    break;
+                }
+
+                redirect(url($gate['redirect']));
             }
         }
 
