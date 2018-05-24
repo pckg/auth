df68fa7744b4063b86702ece9a077d1e7cd14315
Bojan Rajh <schtr4jh@schtr4jh.net>
2018-05-24 23:29:25 +0200

    Remove ip from logging

--- src/Pckg/Auth/Event/Handler/LogUserLogin.php
+++ src/Pckg/Auth/Event/Handler/LogUserLogin.php
@@ -18,7 +18,6 @@ class LogUserLogin extends AbstractChainOfReponsibility
             'hash'     => $_SESSION['Auth']['hash'] ?? null,
             'user_id'  => $user->id,
             'datetime' => date('Y-m-d H:i:s'),
-            'ip'       => server('REMOTE_ADDR', null),
         ]);
 
         return $next();
