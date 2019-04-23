44f0c73e7a18cac94b1d6444a020a1606056e380
Bojan Rajh <schtr4jh@schtr4jh.net>
2019-04-24 01:34:36 +0200

    Create godfather

--- src/Pckg/Auth/Console/CreateGodfather.php
+++ src/Pckg/Auth/Console/CreateGodfather.php
@@ -5,6 +5,7 @@ use Pckg\Auth\Entity\Users;
 use Pckg\Auth\Record\User;
 use Pckg\Auth\Record\UserGroup;
 use Pckg\Framework\Console\Command;
+use Symfony\Component\Console\Input\InputArgument;
 
 class CreateGodfather extends Command
 {
@@ -17,7 +18,8 @@ class CreateGodfather extends Command
                  [
                      'email' => 'Godfather email',
                      'password' => 'Hashed godfather password',
-                 ]
+                 ],
+                 InputArgument::REQUIRED
              );
     }
 
