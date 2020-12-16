8f90fafd9307fd804a1a4b12c9f28539aeac9f4e
Bojan Rajh <schtr4jh@schtr4jh.net>
2020-12-16 16:35:27 +0100

    Conditional auth

--- src/Pckg/Auth/View/advanced.vue
+++ src/Pckg/Auth/View/advanced.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="c-login --advanced flex-tower flex-grid@sm --gap-sm --grid-num --x3@md">
+    <div class="c-login --advanced flex-tower --gap-sm" :class="!noauth && oauth ? 'flex-grid@sm  --grid-num --x3@md' : ''">
 
-        <div class="--center block">
+        <div class="--center block" v-if="!noauth">
             <div class="text-center display-block">
                 <img class="__logo margin-bottom-sm"
                      src="https://startmaestro.cdn.startcomms.com/storage/uploads/backend/commslogo_3_6_4.png"/>
@@ -9,37 +9,37 @@
             <pckg-auth-full :ref="'login'"></pckg-auth-full>
         </div>
 
-        <div class="color-grayish text-center --center font-size-xs block">OR</div>
+        <div class="color-grayish text-center --center font-size-xs block" v-if="!noauth && oauth">OR</div>
 
-        <div class="flex-grid --gap-sm --center --grid-num">
+        <div class="flex-grid --gap-sm --center --grid-num" v-if="oauth">
             <div class="color-grayish text-center --center font-size-xs">Login with OAuth provider</div>
 
-            <a href="/oauth/apple" class="flex-row --start padding-xs-g btn-apple">
+            <a href="/oauth/apple" class="flex-row --start padding-xs-g btn-apple" v-if="hasOauth('apple')">
                 <div class="--center margin-right-xs" style="flex-basis: 3rem;"><i class="fab fa-fw fa-apple"></i>
                 </div>
                 <div class="--center">Login with <b>Apple</b> account</div>
             </a>
 
-            <a href="/oauth/comms" class="flex-row --start padding-xs-g btn-c">
+            <a href="/oauth/comms" class="flex-row --start padding-xs-g btn-c" v-if="hasOauth('comms')">
                 <div class="--center margin-right-xs" style="flex-basis: 3rem;">
                     <img src="https://startmaestro.cdn.startcomms.com/storage/uploads/backend/commslogo_3_6_4.png"/>
                 </div>
                 <div class="--center">Login with <b>Comms ID</b></div>
             </a>
 
-            <a href="/oauth/facebook" class="flex-row --start padding-xs-g btn-fb">
+            <a href="/oauth/facebook" class="flex-row --start padding-xs-g btn-fb" v-if="hasOauth('facebook')">
                 <div class="--center margin-right-xs" style="flex-basis: 3rem;"><i
                     class="fab fa-fw fa-facebook"></i></div>
                 <div class="--center">Login with <b>Facebook</b> account</div>
             </a>
 
-            <a href="/oauth/linkedin" class="flex-row --start padding-xs-g btn-linkedin">
+            <a href="/oauth/linkedin" class="flex-row --start padding-xs-g btn-linkedin" v-if="hasOauth('linkedin')">
                 <div class="--center margin-right-xs" style="flex-basis: 3rem;"><i
                     class="fab fa-fw fa-linkedin"></i></div>
                 <div class="--center">Login with <b>LinkedIn</b> account</div>
             </a>
 
-            <a href="/oauth/google" class="flex-row --start padding-xs-g btn-g">
+            <a href="/oauth/google" class="flex-row --start padding-xs-g btn-g" v-if="hasOauth('google')">
                 <div class="--center margin-right-xs" style="flex-basis: 3rem;"><i class="fab fa-fw fa-google"></i>
                 </div>
                 <div class="--center">Login with <b>Google</b> account</div>
@@ -86,12 +86,14 @@
 
 .btn-c {
     background-color: #0d263d;
+
     img {
         width: 2.4rem;
         vertical-align: middle;
         margin: 0 .3rem;
     }
 }
+
 .__logo {
     max-height: 6.4rem;
     max-width: 6.4rem;
@@ -99,5 +101,19 @@
 </style>
 
 <script>
-export default {}
+export default {
+    props: {
+        oauth: {
+            default: true
+        },
+        noauth: {
+            default: false
+        }
+    },
+    methods: {
+        hasOauth: function (method) {
+            return this.oauth === true || (Array.isArray(this.oauth) && this.oauth.indexOf(method) >= 0);
+        }
+    }
+}
 </script>
